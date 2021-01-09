be23b6bdc7e052dccf1bf0cd0a1d64451eaf7a8d
gcgc112 <2402689824@qq.com>
2021-01-09 16:40:21 +0800

    删除了target目录，新建了test

---
+++ src/main/java/com/offcn/test/Test.java
@@ -0,0 +1,10 @@
+package com.offcn.test;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+
+}
