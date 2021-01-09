ebe1cd19d09e55056b5c7968e73973df820544db
gcgc112 <2402689824@qq.com>
2021-01-09 17:01:12 +0800

    修改Test.java

--- src/main/java/com/offcn/test/Test.java
+++ src/main/java/com/offcn/test/Test.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+
+
     }
 
 }
